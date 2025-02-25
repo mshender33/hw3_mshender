b6605eecf3c196e3899d9881ee3bde7fb55b4a0b
mshender33 Lopthe33# <misha.shender@kellogg.northwestern.edu>
2025-02-25 17:34:22 +0000

    Finished Assignment no decorations

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,10 +1,29 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.all # Fetches all places for the view
+    @places = Place.all  # Ensures @places is always available for index.html.erb
   end
 
   def show
-    @place = Place.find(params[:id])
-    @entries = @place.entries # Fetch entries for the selected place
+    @place = Place.find(params[:id])  # Finds the place by ID
+    @entries = @place.entries         # Fetches all associated entries
+  end
+
+  def new
+    @place = Place.new  # Initializes an empty place object for the form
+  end
+
+  def create
+    @place = Place.new(place_params)
+    if @place.save
+      redirect_to place_path(@place) # Redirects to the newly created place
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name)
   end
 end
